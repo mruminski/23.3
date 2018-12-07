32d79b48372b66dfa8b955529c272161dfdeb9a4
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-12-07 18:40:32 +0100

    Set routes and controllers for lane and note

--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -1,7 +1,12 @@
-import { Router } from 'express';
-import * as NoteController from '../controllers/note.controller';
+import { Router } from "express";
+import * as NoteController from "../controllers/note.controller";
 
 const router = new Router();
 
+router.route("/notes").post(NoteController.addNote);
+
+router.route("/notes/:noteId").put(NoteController.editNote);
+
+router.route("/notes/:noteId").delete(NoteController.deleteNote);
 
 export default router;
